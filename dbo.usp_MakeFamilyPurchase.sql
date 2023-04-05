1da8daed7b55b82387250f00b0e31d308b723a68
GolovinEG <e_golovin@list.ru>
2023-04-05 10:10:18 +0300

    Update dbo.usp_MakeFamilyPurchase.sql

--- dbo.usp_MakeFamilyPurchase.sql
+++ dbo.usp_MakeFamilyPurchase.sql
@@ -1,4 +1,4 @@
-use test;
+﻿use test;
 
 if object_id('MakeFamilyPurchse', 'P') is not null drop proc MakeFamilyPurchse;
 go
@@ -7,8 +7,11 @@ create proc MakeFamilyPurchse
 	@FamilySurName as varchar(255)
 
 as
-		update dbo.Family
-		set BudgetValue = BudgetValue - (select sum(Value)
-										from dbo.Basket
-										where dbo.Basket.ID_Family = dbo.Family.ID)
+		if (select count(*) from dbo.Family where dbo.Family.SurName = @FamilySurName) > 0
+			update dbo.Family
+			set BudgetValue = BudgetValue - (select sum(Value)
+											from dbo.Basket
+											where dbo.Basket.ID_Family = dbo.Family.ID)
+			where dbo.Family.SurName = @FamilySurName;
+		else print(N'Нет семьи с данной фамилией.')
 go
