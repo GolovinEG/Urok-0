e79172bd6bad717abdcc38ee2c968a7938cb3809
GolovinEG <e_golovin@list.ru>
2023-04-07 15:49:40 +0300

    Update dbo.vw_SKUPriceв.sql

--- dbo.vw_SKUPriceв.sql
+++ dbo.vw_SKUPriceв.sql
@@ -1,9 +1,9 @@
 use test;
 
-if object_id('SKUPrice') is not null drop view SKUPrice;
+if object_id('dbo.vw_SKUPrice') is not null drop view dbo.vw_SKUPrice;
 go
 
-create view SKUPrice
+create view dbo.vw_SKUPrice
 as
 select ID, Code, Name, dbo.GetSKUPrice(ID) as Price
 from dbo.SKU
