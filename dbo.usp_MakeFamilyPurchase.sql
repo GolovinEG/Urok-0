3cb0d1df9a7629b1dcefcf3fd29a619efe23bdfa
GolovinEG <e_golovin@list.ru>
2023-04-07 16:15:39 +0300

    Update dbo.usp_MakeFamilyPurchase.sql

--- dbo.usp_MakeFamilyPurchase.sql
+++ dbo.usp_MakeFamilyPurchase.sql
@@ -7,11 +7,24 @@ create proc dbo.usp_MakeFamilyPurchse
 	@FamilySurName as varchar(255)
 
 as
+	begin try
 		if (select count(*) from dbo.Family where dbo.Family.SurName = @FamilySurName) > 0
 			update dbo.Family
 			set BudgetValue = BudgetValue - (select sum(Value)
 											from dbo.Basket
 											where dbo.Basket.ID_Family = dbo.Family.ID)
 			where dbo.Family.SurName = @FamilySurName;
-		else raiserror(N'Нет семьи с данной фамилией.', 0, 1) with nowait
+		else raiserror(N'Нет семьи с данной фамилией.', 16, 1) with nowait
+	end try
+	begin catch
+		declare @ErrorMessage nvarchar(4000);
+		declare @ErrorSeverity int;
+		declare @ErrorState int;
+
+		select
+			@ErrorMessage = error_message(),
+			@ErrorSeverity = error_severity(),
+			@ErrorState = error_state();
+		raiserror(@ErrorMessage, @ErrorSeverity, @ErrorState);
+	end catch;
 go
